d96a0f9fba2a58e1d6e2e79a1fe30ba7c5d396ec
Kapadokia-Titus <ktnyandoch@gmail.com>
2020-10-02 04:41:07 +0300

    still figuring it out

--- app/src/main/java/kapadokia/nyandoro/iq_questions/ui/base/BaseActivity.java
+++ app/src/main/java/kapadokia/nyandoro/iq_questions/ui/base/BaseActivity.java
@@ -1,8 +1,20 @@
 package kapadokia.nyandoro.iq_questions.ui.base;
 
 
+import android.app.ProgressDialog;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.ViewDataBinding;
 
+import javax.inject.Inject;
+
 public abstract class BaseActivity<T extends ViewDataBinding, V extends BaseViewModel> extends AppCompatActivity {
+
+
+    private ProgressDialog mProgressDialog;
+
+    private T mViewDataBinding;
+
+    @Inject
+    protected V mViewModel;
 }
